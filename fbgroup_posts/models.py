7dd8bc72619c9c83186294d03c3932491cee0c78
Rafiqul Hasan <rafiqul@themexpert.com>
2016-12-08 01:09:07 +0600

    Post model created.

--- fbgroup_posts/models.py
+++ fbgroup_posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    post_id = models.CharField('ID', max_length=80)
+    created_time = models.DateTimeField('Created Time')
+    from_name = models.CharField('Name', max_length=80)
+    permalink = models.URLField('Post URL', max_length=255)
+    message = models.TextField()
+    likes = models.IntegerField()
+    comments = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-likes', 'from_name']
+
+    def __str__(self):
+        return self.from_name
+
